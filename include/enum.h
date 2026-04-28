7dfa856860cee843656b0df72808dcd205937107
omarosama16 <omarossamaa2006@gmail.com>
2026-04-28 17:27:37 +0300

    Add enums for OrderStatus and UserType

---
+++ include/enum.h
@@ -0,0 +1,17 @@
+
+#ifndef ENUMS_H
+#define ENUMS_H
+enum class OrderStatus {
+    PENDING,           
+    PREPARING,        
+    OUT_FOR_DELIVERY,   
+    DELIVERED,       
+    CANCELLED          
+};
+
+enum class UserType {
+    CUSTOMER,           
+    DRIVER              
+};
+
+#endif
